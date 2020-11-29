a4735b6bd0c979d8d4c6f2f1f6f20bf5855746ed
anoopsivarajan@outlook.com <anoopsivarajan@outlook.com>
2020-11-29 09:33:29 +0000

    Unit test updates

--- js/modules/k6/dns/dns_test.go
+++ js/modules/k6/dns/dns_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/dop251/goja"
 	"github.com/loadimpact/k6/js/common"
+	miekg "github.com/miekg/dns"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,21 +17,70 @@ func TestDNS_Resolve(t *testing.T) {
 	ctx = common.WithRuntime(ctx, rt)
 	rt.Set("dns", common.Bind(rt, New(), &ctx))
 
-	t.Run("Custom dns", func(t *testing.T) {
-		_, err := common.RunString(rt, `var params={server:'8.8.8.8'};
-		var resp = dns.resolve("google.com",params);
-		if(resp==null){
-			throw new Error("resp is not expected"+resp);
-		}`)
+	dns := New()
+
+	t.Run("Parse data", func(t *testing.T) {
+		resp, err := common.RunString(rt, `dns.sendUDP("google.com","A","8.8.8.8:53")`)
 		assert.NoError(t, err)
+		assert.NotNil(t, resp)
 	})
 
-	t.Run("Default dns", func(t *testing.T) {
-		_, err := common.RunString(rt, `
-		var resp = dns.resolve("google.com");
-		if(resp==null){
-			throw new Error("resp is not expected"+resp);
-		}`)
-		assert.NoError(t, err)
+	t.Run("UDP", func(t *testing.T) {
+		resp, err := dns.SendUDP(ctx, "google.com", "A", "8.8.8.8:53")
+		assert.Nil(t, err)
+		assert.NotNil(t, resp)
+	})
+
+	t.Run("Parse data", func(t *testing.T) {
+		question := make(map[string]interface{})
+		question["domain"] = "google.com"
+		question["qType"] = "A"
+
+		data := parseData(question)
+		assert.Equal(t, "google.com", data.Domain)
+		assert.Equal(t, "A", data.QType)
+	})
+
+	t.Run("Build message", func(t *testing.T) {
+		question := make(map[string]interface{})
+		question["domain"] = "google.com"
+		question["qType"] = "A"
+
+		resp := dns.BuildMessage(ctx, question)
+		assert.NotNil(t, resp)
+		assert.NotEmpty(t, resp)
+	})
+
+	t.Run("Get type from string", func(t *testing.T) {
+		qt := getTypeFromString("CNAME")
+		assert.Equal(t, miekg.TypeCNAME, qt)
+
+		qt = getTypeFromString("ahsss")
+		assert.Equal(t, miekg.TypeA, qt)
+	})
+
+	t.Run("Create message", func(t *testing.T) {
+		msg := createMessage("bbc.co.uk", "CNAME")
+		assert.Equal(t, 1, len(msg.Question))
+		q := msg.Question[0]
+		assert.Equal(t, "bbc.co.uk.", q.Name)
+		assert.Equal(t, uint16(5), q.Qtype)
+	})
+
+	t.Run("Unpack message", func(t *testing.T) {
+
+		question := make(map[string]interface{})
+		question["domain"] = "bbc.co.uk"
+		question["qType"] = "A"
+
+		resp := dns.BuildMessage(ctx, question)
+
+		msg := dns.UnpackMessage(ctx, resp)
+
+		assert.Equal(t, 1, len(msg.Question))
+		q := msg.Question[0]
+		assert.Equal(t, "bbc.co.uk.", q.Name)
+		assert.Equal(t, uint16(1), q.Qtype)
+
 	})
 }
